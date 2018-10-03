de4e6e6fffaa9716a8273f2d765c5a324ba4fd62
Maksym <mnaichuk@heliostech.fr>
2018-10-03 14:06:56 +0300

    Update parser_csvXRP.rb

--- config/parser_csvXRP.rb
+++ config/parser_csvXRP.rb
@@ -17,10 +17,6 @@ fields = doc.shift
 records = Array.new
 last_order_time = 0
 doc.each_with_index do |row, i|
-  if (row[0].to_i - last_order_time).abs < 1*60
-    next
-  end
-  last_order_time = row[0].to_i
   count += 1
   record = Array.new
   fields.each_with_index do |field, j|
